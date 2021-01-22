7932763e286e72e47fd3a782ff7460e2f827efe3
Guillaume Outters <guillaume-sqleur@outters.eu>
2021-01-22 02:29:18 +0100

    detroussages
    + Sur un groupe d'entrée, complète les null des unes avec les valeurs des autres.
    - Uniquement l'implémentation PostgreSQL pour le moment.
    - Ne sait pas encore faire d'exclusion de colonnes.
    - Ne sait pas encore ajouter un bout de code personnalisé (pour par exemple historiser les modifications avant leur modification, renvoyer un texte différent, etc.).
    
    darcs-hash:87aa6efd2795d097b77514b723c82f34f53ab06a

---
+++ detroussages.pg.fonc.sql
@@ -0,0 +1,77 @@
+-- Copyright (c) 2020-2021 Guillaume Outters
+--
+-- Permission is hereby granted, free of charge, to any person obtaining a copy
+-- of this software and associated documentation files (the "Software"), to deal
+-- in the Software without restriction, including without limitation the rights
+-- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+-- copies of the Software, and to permit persons to whom the Software is
+-- furnished to do so, subject to the following conditions:
+--
+-- The above copyright notice and this permission notice shall be included in
+-- all copies or substantial portions of the Software.
+--
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+-- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+-- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+-- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+-- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+-- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+-- SOFTWARE.
+
+#define for_COLONNE_in_cols \
+	$$||(select string_agg(replace($$
+#define done_COLONNE_in_cols \
+	$$, 'COLONNE', col), E'\n') from unnest(cols) t(col))||$$
+
+with
+	taches as
+	(
+		select row_number() over() tache, string_to_array(g, ' ')::bigint[] ids from unnest(groupes) t(g)
+	),
+	mamie as
+	(
+		select
+			tache, ids
+			for_COLONNE_in_cols
+			, max(_source.COLONNE) COLONNE_max, min(_source.COLONNE) COLONNE_min
+			done_COLONNE_in_cols
+		from taches join $$||nomTable||$$ _source on _source.id = any(taches.ids)
+		group by 1, 2
+		having count(1) > 1 -- Inutile de comparer une entrée toute seule avec elle-même.
+	),
+	-- Sur quels champs les entrées possédant de la donnée se mettent-elles d'accord?
+	daccord as
+	(
+		select
+			tache, ids
+			for_COLONNE_in_cols
+			, case when COLONNE_max = COLONNE_min then COLONNE_max end COLONNE
+			done_COLONNE_in_cols
+		from mamie
+	),
+	afaire as
+	(
+		select
+			daccord.tache, _source.id
+			for_COLONNE_in_cols
+			, coalesce(_source.COLONNE, daccord.COLONNE) COLONNE
+			done_COLONNE_in_cols
+		from daccord join $$||nomTable||$$ _source on _source.id = any(ids)
+		where false
+		-- Ne sélectionnons l'entrée que si au moins un de ses champs va être modifié.
+		for_COLONNE_in_cols
+		or (_source.COLONNE is null and daccord.COLONNE is not null)
+		done_COLONNE_in_cols
+	),
+	maj as
+	(
+		update $$||nomTable||$$ _source
+		set
+			id = _source.id -- Histoire de pouvoir commencer par une virgule ensuite.
+			for_COLONNE_in_cols
+			, COLONNE = afaire.COLONNE
+			done_COLONNE_in_cols
+		from afaire
+		where _source.id = afaire.id
+		returning afaire.tache, _source.id, null::text[] -- À FAIRE: remonter la liste des champs complétés.
+	)
---
+++ detroussages.pg.sql
@@ -0,0 +1,61 @@
+-- Copyright (c) 2021 Guillaume Outters
+--
+-- Permission is hereby granted, free of charge, to any person obtaining a copy
+-- of this software and associated documentation files (the "Software"), to deal
+-- in the Software without restriction, including without limitation the rights
+-- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+-- copies of the Software, and to permit persons to whom the Software is
+-- furnished to do so, subject to the following conditions:
+--
+-- The above copyright notice and this permission notice shall be included in
+-- all copies or substantial portions of the Software.
+--
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+-- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+-- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+-- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+-- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+-- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+-- SOFTWARE.
+
+-- DéTrousSages
+-- Supprime les trous d'entrées à partir d'une autre entrée de la même table.
+-- Est considéré comme un trou une colonne à null (à moins que le null n'y soit référencé comme significatif)
+
+#if defined(DETROU_COLONNES_IGNOREES)
+-- À FAIRE
+#endif
+
+create or replace function detroussages(nomTable text, groupes text[], perso text) returns table(tache bigint, id bigint, champs text[]) language plpgsql as
+$$
+	begin
+		execute detroussages_fonc_table(nomTable, perso);
+		return query select * from _detroussages_fonc(groupes);
+		drop function _detroussages_fonc(groupes text[]);
+	end;
+$$;
+
+create or replace function detroussages_fonc_table(nomTable text, perso text) returns text language plpgsql as
+$dft$
+	declare
+		cols text[];
+	begin
+		select array_agg(column_name::text) into cols from information_schema.columns
+		where nomTable in (table_name, table_schema||'.'||table_name)
+		and is_nullable = 'YES'
+		and column_name not in ('id')
+		-- À FAIRE: ajouter les exclusions
+		-- À FAIRE: permettre, colonne par colonne, d'avoir une autre valeur "insignifiante" (ex.: '', '-').
+		;
+		
+		return
+			$$
+-- À FAIRE: décoder en dur le nom de la fonction générée, afin d'éviter les interblocages entre sessions faisant simultanément des detroussages.
+create or replace function _detroussages_fonc(groupes text[]) returns table(tache bigint, id bigint, champs text[]) language sql as
+$df$
+#include detroussages.pg.fonc.sql
+select * from maj;
+$df$;
+			$$;
+	end;
+$dft$;
