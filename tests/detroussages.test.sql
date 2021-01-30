d5f761df42e35e09d4b426e277396e40a5030191
Guillaume Outters <guillaume-sqleur@outters.eu>
2021-01-30 23:23:04 +0100

    detroussages.test.sql: rapatriement du résultat
    = Le résultat attendu est placé directement sous le SQL de test.
    + Possibilité de jouer le machine sous phpunit: bdd="pgsql:host=localhost;etc." phpunit ../../sqleur/SqleurTestSuite.php detroussages.test.sql
    
    darcs-hash:a73a715b3039cb59d3e8780581dbffed46e7d404

--- tests/detroussages.test.sql
+++ tests/detroussages.test.sql
@@ -23,7 +23,29 @@ insert into t (num, truc, nombre, quand) values ('3', 'bh', null, null);
 insert into t (num, truc, nombre, quand) values ('3', 'ch', 2, '1979-10-10 08:30:00');
 insert into t (num, truc, nombre) values ('seule', 'borf', null);
 
+#test
 with
 	ids as (select string_agg(id::text, ' ') g from t group by num)
 select * from detroussages('t', (select array_agg(g) from ids), null);
+$$
+1	1	"détroué: nombre"
+1	2	"détroué: truc"
+5	3	"détroué: truc nombre"
+3	5	"détroué: truc"
+4	8	"détroué: quand"
+$$;
+
+#test
 select * from t order by id;
+$$
+1	"1 partout"	coucou	123	-
+2	"1 partout"	coucou	123	-
+3	"2 sur 0"	bah	123	-
+4	"2 sur 0"	bah	123	-
+5	"1 sur 0"	""	1	-
+6	"1 sur 0"	""	1	-
+7	3	ah	1	"1979-10-10 08:30:00"
+8	3	bh	-	"1979-10-10 08:30:00"
+9	3	ch	2	"1979-10-10 08:30:00"
+10	seule	borf	-	-
+$$;
