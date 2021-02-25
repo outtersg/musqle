d84ccf5bef33004aef633aa2eef6371a6c04afdf
Guillaume Outters <guillaume-sqleur@outters.eu>
2021-02-25 23:23:51 +0100

    BOGUE: sudo: confusion nomTableSudoers et nomFonctionSudo
    # La fonction n'a pas pour nom celui de la table de paramétrage.
    = Réorganisation pour tout mettre dans le même schéma (en extrayant le nom de ce dernier une fois pour toute du nom de la table de paramétrage).
    
    darcs-hash:3d713a10910ba883718304c49178d782a77332e8

--- sudo.pg.sql
+++ sudo.pg.sql
@@ -29,14 +29,14 @@
 -- À FAIRE: traces. Bien noter tout de même que le système (de trace dans une table) est faillible, puisqu'il suffit à l'appelant de rollbacker pour ne pas être fliqué (l'entrée en table traces disparaîtrait avec le rollback).
 -- À FAIRE: possibilité de renvoyer le résultat d'un sudo select?
 
-create or replace function _sudo_def(nomTableSudoers text, nomTableTraces text) returns text language sql as
+create or replace function _sudo_def(nomFonctionSudo text, nomTableSudoers text, nomTableTraces text) returns text language sql as
 $bla$
 	select
 	$$
 		-- Balancer une exception eût été mieux, cependant cela nous garantit qu'on perd notre trace (rollback implicite).
 		-- Alors qu'en renvoyant un booléen, la trace n'est perdue que si l'appelant oublie de rollbacker sur retour false.
 		-- L'appelant a donc toujours la possibilité de se masquer, mais uniquement sur furtivité volontaire (certes ce sont ceux-là qu'on voudrait intercepter avant tout…).
-		create or replace function $$||nomTableSudoers||$$(commande text) returns boolean language plpgsql as
+		create or replace function $$||nomFonctionSudo||$$(commande text) returns boolean language plpgsql as
 		$corps$
 			begin
 				
@@ -47,15 +47,17 @@ $bla$;
 
 create or replace function _sudo_installer(nomTableSudoers text, nomTableTraces text) returns void language plpgsql as
 $$
+	declare
+		nomSchema text;
 	begin
 		-- À FAIRE: si nomTable ne contient pas de ., le préfixer du premier composant du search_path. Mais bon ce n'est pas bien de passer par un nom non qualifié: ça pourrait valoir un raise exception.
-		perform _sudo_verifierSchemaDe(nomTableSudoers);
+		select _sudo_verifierSchemaDe(nomTableSudoers) into nomSchema;
 		perform _sudo_verifierTable(nomTableSudoers, 'command text, flags text, cond text');
-		perform _sudo_def(nomTableSudoers, nomTableTraces);
+		execute _sudo_def(nomSchema||'.sudo', nomTableSudoers, nomTableTraces);
 	end;
 $$;
 
-create or replace function _sudo_verifierSchemaDe(nomTable text) returns void language plpgsql as
+create or replace function _sudo_verifierSchemaDe(nomTable text) returns text language plpgsql as
 $$
 	declare
 		nomSchema text;
@@ -67,6 +69,7 @@ $$
 		else
 			-- À FAIRE: s'assurer qu'il est bien configuré en droits, ou au moins péter une exception s'il ne nous appartient pas.
 		end if;
+		return nomSchema;
 	end;
 $$;
 
@@ -87,4 +90,4 @@ select _sudo_installer(SUDOERS, SUDO_TRACES);
 drop function _sudo_verifierTable(text, text);
 drop function _sudo_verifierSchemaDe(text);
 drop function _sudo_installer(text, text);
-drop function _sudo_def(text, text);
+drop function _sudo_def(text, text, text);
