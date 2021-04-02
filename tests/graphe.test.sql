023cc32754dd1258267d5a3d5faf16b07ef6bdc6
Guillaume Outters <guillaume-sqleur@outters.eu>
2021-04-03 01:41:56 +0200

    tests/graphe: d'une fonction
    = On teste aussi ça.
    
    darcs-hash:69f7a97c5d9f282d28c2d53f4f1c57e5d0d22f7e

--- tests/graphe.test.sql
+++ tests/graphe.test.sql
@@ -3,3 +3,20 @@
 #format delim \t
 
 select graphe from graphe('{{4,4,4,5.25,6.5,7,8,7.5,7},{3,3.5,4,3,2,2.2,2.5,3.2,4}}', '{0,ANSI}');
+
+create temporary table t_graphe as
+	select *
+	from
+	(values
+		('oui', 0, 4),
+		('oui', 1, 6),
+		('oui', 2, 8),
+		('oui', 3, 5),
+		('oui', 4, 2),
+		('non', 0, 4),
+		('non', 1, 3),
+		('non', 2, 2),
+		('non', 3, 3.5),
+		('non', 4, 2)
+	) t(groupe, x, y);
+select graphe from graphe('t_graphe', 'groupe', 'x', 'y', '{0,ANSI}');
