8f78c6bd7a67a099a7b35aa29834c598dd12e9bf
Guillaume Outters <guillaume-sqleur@outters.eu>
2021-06-30 22:37:40 +0200

    util.oracle.sh: quelletable()
    + quelletable(): lister les tables contenant un certain identifiant.
    = Par défaut colonne row_id (utilisé par exemple sur du Siebel, où le RowID est unique sur toute la base).
    
    darcs-hash:59c2bea6f730f230406909547af9239bc68e7739

--- util.oracle.sh
+++ util.oracle.sh
@@ -11,3 +11,32 @@ sqloracle()
 	
 	sqlplus -s $BDD_QUI/$BDD_MDP@"(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=$BDD_HOTE)(PORT=$BDD_PORT))(CONNECT_DATA=(SID=$BDD_NOM)))"
 }
+
+# Dans quelle table figure tel RowID?
+# https://chartio.com/resources/tutorials/how-to-list-all-tables-in-oracle/
+quelletable()
+{
+	c=row_id
+	{
+		cat <<TERMINE
+set pagesize 0
+
+select table_name
+from all_tab_columns
+where
+	owner = '$BDD_QUI' and lower(column_name) = lower('$c')
+order by table_name desc;
+TERMINE
+	} | $BDD_SQLEUR | awk '{f="/tmp/temp.tables."(NR%4);print>f}'
+	for f in /tmp/temp.tables.[0123]
+	do
+		{
+			echo "set pagesize 0;"
+			while read t
+			do
+				echo "select 'oui', '$t', $c from $t where $c in ('$1');"
+			done < $f
+		} | $BDD_SQLEUR 2>&1 | grep "^oui" &
+	done
+	wait
+}
