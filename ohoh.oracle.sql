e735c620e3c86c0fee5f9b1a11145891dd80d3e2
Guillaume Outters <guillaume-sqleur@outters.eu>
2023-04-01 06:18:37 +0200

    ohoh.oracle: colonnes LONG
    # Oracle refuse de reporter telle quelle une colonne de type LONG. On ruse.
    
    darcs-hash:4a1ccf369a08680d43b7036129ad85bb496df800

--- ohoh.oracle.sql
+++ ohoh.oracle.sql
@@ -45,11 +45,14 @@
 #endif
 
 -- À FAIRE: la version avec OHOH_MAJ pour mettre à jour une entrée récente plutôt que d'en rajouter une. Cf. ohoh.pg.sql.
+-- À FAIRE: émettre des fonctions précompilées pour chaque table.
+--          Problème: comment détecter que la table a changé si c'est une version énumérant les colonnes (alors que le t.* nous assure que notre fonction est toujours alignée sur la table réelle)?
 
 create or replace function LOCAL(ohoh)(nomTable varchar2, ancien DEDE_ID_TYPE, nouveau DEDE_ID_TYPE, commentaire varchar2)
 	return integer
 as
 	r integer;
+	colonnes clob;
 	-- https://stackoverflow.com/a/38610104
 	table_does_not_exist exception;
 	pragma exception_init(table_does_not_exist, -942);
@@ -59,10 +62,11 @@ as
 		select LOCAL(ohoh_)(nomTable, ancien, nouveau, commentaire) into r from dual;
 		return r;
 	exception when table_does_not_exist then
+		select LOCAL(ohoh_colonnes(nomTable)) into colonnes from dual;
 		execute immediate
 		'
 			create table '||nomTable||'OHOH_SUFFIXE as
-				select OHOH_COLS_DEF, t.* from '||nomTable||' t where rownum < 0
+				select OHOH_COLS_DEF, '||colonnes||' from '||nomTable||' t where rownum < 0
 		'
 		;
 		execute immediate 'create index '||nomTable||'OHOH_SUFFIXE'||'_id_i on '||nomTable||'OHOH_SUFFIXE(DEDE_ID)';
@@ -78,16 +82,62 @@ as
 create or replace function LOCAL(ohoh_)(nomTable in varchar2, ancien in DEDE_ID_TYPE, nouveau in DEDE_ID_TYPE, commentaire in varchar2)
 	return integer
 as
+	colonnes clob;
 	-- https://stackoverflow.com/a/8729553
 	-- Bien penser au commit (sans quoi ORA-06519: transaction autonome active détectée et annulée).
 	pragma autonomous_transaction;
 	begin
+		select LOCAL(ohoh_colonnes(nomTable)) into colonnes from dual;
 		execute immediate
 		'
 			insert into '||nomTable||'OHOH_SUFFIXE
-				select '||replace('OHOH_COLS', 'nouveau', ''''||nouveau||'''')||', t.* from '||nomTable||' t where DEDE_ID in ('''||ancien||''')
+				select '||replace('OHOH_COLS', 'nouveau', ''''||nouveau||'''')||', '||colonnes||' from '||nomTable||' t where DEDE_ID in ('''||ancien||''')
 		';
 		commit;
 		return 1;
+	end;
+/
+
+-- Perfs:
+-- 3,1 s lower(table_name) = lower(x) or lower(owner||'.'||table_name) = lower(x) -- Adaptabilité maximale.
+-- 2,5 s sans les lower (si on écrit déjà dans la bonne casse)
+-- 0,3 s juste sur le nom de table.
+-- Bref pour une question de perfs, merci d'appeler ohoh_colonnes avec le nom de table sans le schéma!
+
+#define TABLE_NOMMÉE(x) (lower(table_name) = lower(x) or lower(owner||'.'||table_name) = lower(x))
+#define TABLE_NOMMÉE(x) table_name = upper(x)
+
+-- https://stackoverflow.com/questions/29116396/workaround-for-ora-00997-illegal-use-of-long-datatype
+create or replace function LOCAL(ohoh_colonnes)(nomTable in varchar2)
+	return clob
+as
+	colonnes clob;
+	versionLongue integer;
+	begin
+		select
+			case when exists (select 1 from all_tab_columns where TABLE_NOMMÉE(nomTable) and data_type in ('LONG')) then 1
+			else 0
+			end
+		into versionLongue from dual;
+		if versionLongue = 0 then return 't.*'; end if;
+		
+		-- listagg est soumis à la limite des 4000 caractères, même sur du clob pour produire du clob (list_agg(to_clob(…)), grrr…
+		-- https://stackoverflow.com/questions/74037009/how-i-can-use-listagg-with-clob
+		-- On doit donc boucler sur des paquets de moins de 4000 caractères et agréger nous-mêmes, lorsque le schéma est vraiment, vraiment chargé.
+		-- À FAIRE: version mi-longue si sum(length(column_name)) < 3000 (de la marge pour inclure les to_lob()).
+		colonnes := '';
+		for colonne in
+		(
+			select
+				listagg(case when data_type = 'LONG' then 'to_lob('||column_name||') '||column_name else column_name end, ',') within group (order by column_id) trad
+			from all_tab_columns
+			where TABLE_NOMMÉE(nomTable)
+			group by floor(column_id / 100)
+		)
+		loop
+			if length(colonnes) > 0 then colonnes := colonnes||','; end if;
+			colonnes := colonnes||colonne.trad;
+		end loop;
+		return colonnes;
 	end;
 /
