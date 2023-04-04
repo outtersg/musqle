11ba7338d6dc7a89b4747437f9c0f05d660c057e
Guillaume Outters <guillaume-sqleur@outters.eu>
2023-04-04 06:04:37 +0200

    rade: rade_init pour Oracle
    + Adaptation costaude (avec beaucoup de manuel, mais après tout les gens qui bossent sous Oracle aiment ça).
    = Moins de champs illimités (PostgreSQL préfère, mais Oracle, bof bof. Et les autres on ne sait pas).
    
    darcs-hash:92b570ca4df597078952a9f165890ce8bc5bcf3e

--- rade_init.sql
+++ rade_init.sql
@@ -20,21 +20,33 @@
 
 -- /!\ Fichier technique, inclus par rade.sql (qui contient toutes les définitions nécessaires). Ne pas inclure directement.
 
-#if `select count(*) from pg_tables where tablename = 'RADE_REF'` = 0
+#if :driver = "pgsql"
+#set RADE_DEJA `select count(*) from pg_tables where tablename = 'RADE_REF'`
+#elif !defined(RADE_DEJA)
+#set RADE_DEJA 0
+#endif
+
+#if !RADE_DEJA
 
 create table RADE_REF
 (
 	id AUTOPRIMARY,
-	indicateur T_TEXT,
-	producteur T_TEXT,
+	indicateur T_TEXT(255),
+	producteur T_TEXT(255),
+#if :driver = "oracle"
+	delai_alerte interval day to second, -- not null default interval '0' second, -- En Oracle 12?
+	delai_retention interval day to second -- not null default interval '36' hour -- En Oracle 12?
+#else
 	delai_alerte interval not null default '0s',
 	delai_retention interval not null default '36h'
+#endif
 );
+AUTOPRIMARY_INIT(RADE_REF, id)
 -- À FAIRE: contrainte pour que le delai_retention soit toujours supérieur au delai_alerte (difficile d'alerter sur une info qu'on a perdue…).
 
 create table RADE_DETAIL
 (
-	id T_TEXT primary key,
+	id T_TEXT(255) primary key,
 	indicateur_id integer not null references RADE_REF(id),
 	de timestamp not null,
 	a timestamp,
@@ -48,5 +60,6 @@ create table RADE_STATS
 	de timestamp not null,
 	n integer not null
 );
+AUTOPRIMARY_INIT(RADE_STATS, id)
 
 #endif
