25eeef53fd611721927acd44a2fec1689bf0ca47
Guillaume Outters <guillaume-sqleur@outters.eu>
2023-08-26 00:11:43 +0200

    util.chaines: EXPR_ACCOLADES_x
    + Repérage de blocs d'accolades imbriquées.
    
    darcs-hash:be4c09eaf2ed9c439b5909c7587dd1ce5ff744db

--- util.chaines.sql
+++ util.chaines.sql
@@ -20,4 +20,48 @@
 -- SOFTWARE.
 
 -- N.B.: ce fichier étant destiné à être inclus, l'ensemble de ses commentaires est encadré de #if 0 pour éviter de polluer l'incluant de nos notes.
+
+--------------------------------------------------------------------------------
+-- Regexp
+
+-- Suppression des accolades imbriquées ----------------------------------------
+
+-- Les expressions EXPR_ACCOLADES_x repèrent les accolades de premier niveau,
+-- avec tout leur contenu y compris accolades imbriquées,
+-- mais se gardant d'interpréter celles incluses dans des chaînes guillemetées.
+-- Le x est le niveau maximal d'imbrication gérable.
+-- Permet de travailler sur du JSON ou du PHP serialize.
+
+#endif
+#define EXPR_ACCOLADES_ {([^{}"]+|"([^"\\]+|\\.)*"|@)*}
+#define EXPR_ACCOLADES_ECO_ {(?:[^{}"]+|"(?:[^"\\]+|\\.)*"|@)*}
+#if defined(EXPR_ACCOLADES_ECO) or (defined(:pilote) and :pilote in ("pgsql"))
+#define EXPR_ACCOLADES_ EXPR_ACCOLADES_ECO_
+#endif
+#set EXPR_ACCOLADES_1 replace(EXPR_ACCOLADES_, "|@", "")
+#set EXPR_ACCOLADES_2 replace(EXPR_ACCOLADES_, "@", EXPR_ACCOLADES_1)
+#set EXPR_ACCOLADES_3 replace(EXPR_ACCOLADES_, "@", EXPR_ACCOLADES_2)
+#set EXPR_ACCOLADES_4 replace(EXPR_ACCOLADES_, "@", EXPR_ACCOLADES_3)
+#if 0
+
+#if 0
+-- Utilisation, sur du PHP serialize:
+select regexp_replace('{ezcnl;"àfairesauter";a:1234:{coucou"coucou{salu\"{t":{cnl};miam:{bla:{"coucou{salu\"{t"}}};suite{oui}', '"àfairesauter";a:[0-9]+:EXPR_ACCOLADES_4;?', '');
+#endif
+
+#if 0
+-- Une version pur SQL, avec with recursive, peut se définir ainsi:
+with recursive
+	niveau as (select 3 nmax),
+	exprs as
+	(
+		select '{(?:[^{}"]+|"(?:[^"\\]+|\\.)*"@)*}' e, 1 niveau
+		union
+		select replace(e, '@', case niveau when nmax then '' else '|'||e end), niveau + 1 from niveau, exprs
+		where niveau <= nmax
+	),
+	expr as (select e from niveau, exprs where niveau > nmax)
+select * from expr;
+#endif
+
 #endif
