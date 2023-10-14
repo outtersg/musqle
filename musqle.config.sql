c457f47acacf841de3e20bda2189249f7ad89a85
Guillaume Outters <guillaume-sqleur@outters.eu>
2023-10-14 08:29:53 +0200

    mutualisation dans les .config.sql
    = Reprise de diverses définitions standard d'autres fichiers:
    = DEDE_ID_TYPE.
    = DEDE_SCHEMA, :SCHEMA.
    = LOCAL(t).
    = OHOH_SUFFIXE, OHOH_CLE, OHOH_COLQ.
    
    darcs-hash:b047921bbef4b47d42bfa505bbe235a81b2cad2f

--- musqle.config.sql
+++ musqle.config.sql
@@ -2,9 +2,25 @@
 #define DEDE_ID id
 #endif
 #if not defined(DEDE_ID_TYPE)
+#if defined(:pilote) and :pilote == "pgsql"
 #define DEDE_ID_TYPE bigint
+#else
+#define DEDE_ID_TYPE integer
+#endif
 #endif
 
-#if defined(:pilote) and :pilote == "pgsql"
+#if defined(DEDE_SCHEMA) and not defined(:SCHEMA)
+#define :SCHEMA DEDE_SCHEMA
+#endif
+#if defined(:pilote) and :pilote == "pgsql" and not defined(:SCHEMA)
 #set :SCHEMA `select current_schema`
+#endif
+#if defined(:SCHEMA) and not defined(DEDE_SCHEMA)
+#define DEDE_SCHEMA :SCHEMA
+#endif
+
+#if defined(DEDE_SCHEMA)
+#define LOCAL(t) DEDE_SCHEMA.t
+#else
+#define LOCAL(t) t
 #endif
