9b9c8c348d89ead18c271dcc0c7adfb35a370a5f
Guillaume Outters <guillaume-sqleur@outters.eu>
2023-10-14 08:29:53 +0200

    mutualisation dans les .config.sql
    = Reprise de diverses définitions standard d'autres fichiers:
    = DEDE_ID_TYPE.
    = DEDE_SCHEMA, :SCHEMA.
    = LOCAL(t).
    = OHOH_SUFFIXE, OHOH_CLE, OHOH_COLQ.
    
    darcs-hash:b047921bbef4b47d42bfa505bbe235a81b2cad2f

---
+++ ohoh.config.sql
@@ -0,0 +1,10 @@
+#if not defined(OHOH_SUFFIXE)
+#define OHOH_SUFFIXE _poubelle
+#endif
+
+#if not defined(OHOH_CLE)
+#define OHOH_CLE id, rempl_cause
+#endif
+#if not defined(OHOH_COLQ)
+#define OHOH_COLQ rempl_date
+#endif
