7be05580453f4b3d7644c1ad7e66f3ab0d9cd080
Guillaume Outters <guillaume-sqleur@outters.eu>
2024-03-30 22:18:22 +0100

    util.oracle.sql: EXTASE: EXtract Table As Select
    = Façon toute simple d'extraire une table de données volumineuses, avec synthèse de la doc trouvée sur le sujet et d'expérimentations sur un Oracle 11.2.
    
    darcs-hash:61033b016b01feb38e03368aff9ba6ed0741cf8e

--- util.oracle.sql
+++ util.oracle.sql
@@ -55,3 +55,27 @@ $$;
 		_drop_sequence_if_exists('nom_sequence',); \
 	end;
 #define /drop sequence if exists ([^ ;]*)/i drop_sequence_if_exists(\1)
+#if ___NOTE___
+
+--------------------------------------------------------------------------------
+-- Chargement en masse
+
+-- forall équivalent à SQL*Loader (10 - 12 s), puis Import (20 s), Insert (60 s)   # http://www.dba-oracle.com/art_orafaq_data_load.htm
+-- ex. forall   # https://blogs.oracle.com/connect/post/bulk-processing-with-bulk-collect-and-forall
+-- CREATE TABLE tbl AS SELECT (CTAS) with PARALLEL (DEGREE x)   # https://stackoverflow.com/a/76305851/1346819
+-- Table externe CSV   # https://asktom.oracle.com/ords/f?p=100:11:::::P11_QUESTION_ID:1615330671789
+-- insert select aussi bon que le plus optimisé des forall   # https://livesql.oracle.com/apex/livesql/file/content_CTTBPHPAOQNA0KQ1J93TTRREB.html
+-- insert /*+ append */   # https://asktom.oracle.com/ords/f?p=100:11:::::p11_question_id:1415454871121
+-- insert append parallel   # https://stackoverflow.com/a/10424916/1346819
+-- Un bon résumé   # https://www.bobbydurrettdba.com/2012/06/21/fast-way-to-copy-data-into-a-table/
+-- Attention, plein de contraintes sur la transaction (https://www.oreilly.com/library/view/oracle-parallel-processing/156592701X/ch04s02.html)
+-- mais si nous fermons la transaction ça devrait être bon.
+-- Voir aussi les Index-Organized Tables (un poil des données entreposées directement dans l'index).
+
+-- EXTASE: EXtract Table As Select, Enhanced
+-- Le plus rapide et simple reste le CTAS; l'insert /*+ append */ après avoir créé la table est sensiblement équivalent mais perd petit à petit dans les grands volumes.
+#endif
+#define create_table_as(TABLE, SELECT) \
+	create table TABLE parallel 8 nologging as\
+		SELECT
+#define /(?:^|\n)EXTASE\s+(\w+)\s+(.+)/s create_table_as(\1, \2)
