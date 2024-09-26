2268a9421235fbe8466271b5af8eec891aa87256
Guillaume Outters <guillaume-sqleur@outters.eu>
2024-09-26 23:32:50 +0200

    xavier()
    + Reconstruction de balises fermantes sur un XML tronqué.
    - Pour le moment seulement une implémentation PostgreSQL.
    
    darcs-hash:36a15ec7923900c9c303b8d861a10e9f7f036990

--- xml.sql
+++ xml.sql
@@ -21,4 +21,58 @@
 -- SOFTWARE.
 
 -- N.B.: ce fichier étant destiné à être inclus, l'ensemble de ses commentaires est encadré de #if 0 pour éviter de polluer l'incluant de nos notes.
+
+-- XAVIER ----------------------------------------------------------------------
+-- XAVIER: Xml À Valider Incomplet: Étude et Reconstruction
+-- BMX: Balises Manquantes Xml
+-- BOXER: Balises Orphelines XML En Reconstruction
+-- Sur un XML qui a été tronqué, reconstruit les balises fermantes pour en faire au moins un XML valide.
+#endif
+create or replace function xavier(xml text) returns text language plpgsql as
+$$
+	declare
+		r text; -- Reste.
+		b0 text; -- Bout.
+		b1 text;
+		p integer;
+	begin
+		-- On élimine:
+		-- - toute balise ouvrante non fermée, ex.: <déb
+		-- - toute balise non terminée, ex.: <date>1979-1
+		-- mais on garde:
+		-- - un début de balise fermante, qu'on suppose suivre un contenu complet, ex.: <morceau>contenu</mo
+		-- - une balise ouvrante suivie d'une sous-balise
+		r := regexp_replace(xml, '(?:<[^/][^>*]*[^/]>|</[^<]*|<[^/][^>]*|<[^>]*[^/]>[^<]*)$', '');
+		b0 := replace(r, chr(10), ' ');
+		b0 := regexp_replace(b0, '<[^>]+/>|<[?][^>]*[?]>', '', 'g'); -- Les <balise/> et <?xml …?>
+		b0 := regexp_replace(b0, '>[^<]+<', '><', 'g'); -- Le contenu textuel
+		b0 := regexp_replace(b0, '<([^> ]+)[ ][^>]*[^/>]>', '<\1>', 'g'); -- Les attributs
+		while b1 is null or length(b0) < length(b1) loop
+			b1 := b0;
+			b0 := regexp_replace(b0, '<[^/][^>]*></[^>]*>', '', 'g');
+		end loop;
+		b0 := '';
+		loop
+			p := position('>' in b1); -- position, locate, instr, charindex…
+			exit when p = 0;
+			b0 := '</'||substr(b1, 2, p - 1)||b0;
+			b1 := substr(b1, p + 1);
+		end loop;
+		return r||b0;
+	end;
+$$;
+#if ___NOTE___
+-- { echo "#format delim \\t" ; echo "#include xml.sql" ; awk '/<\/test>/{oui=0}oui{print}/<test>/{oui=1}' < xml.sql ; } | bdd='pgsql:host=localhost port=5432 dbname=test' php ../sqleur/sql2csv.php
+-- <test>
+with
+	t as
+	(
+			  select '<?xml blabla?><début><milieu><fi' e,                    '<?xml blabla?><début><milieu></milieu></début>' a
+		union select '<?xml bla?><début><milieu>oui</milieu><autre>non</au',  '<?xml bla?><début><milieu>oui</milieu><autre>non</autre></début>'
+		union select '<?xml bla?><début><milieu>oui</milieu><autre>no',       '<?xml bla?><début><milieu>oui</milieu></début>'
+		union select '<?xml bla?><début><milieu>oui</milieu><autre><oui/><a', '<?xml bla?><début><milieu>oui</milieu><autre><oui/></autre></début>'
+	),
+	x as (select t.*, xavier(e) r from t)
+select case when a = r then '[32moui[0m' else '[31mnon[0m' end bon, e entree, a attendu, r recu from x;
+-- </test>
 #endif
